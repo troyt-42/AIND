dcbcab88b7198ce984bf530655c0b5fb523af6bd
Tao_Mac <rnm2888@gmail.com>
2017-01-23 23:56:03 -0500

    Add a hard sudoku

--- soduku/function.py
+++ soduku/function.py
@@ -65,5 +65,9 @@ def reduce_puzzle(values):
         if len([box for box in values.keys() if len(values[box]) == 0]):
             return False
     return values
-
-display(reduce_puzzle(grid_values('..3.2.6..9..3.5..1..18.64....81.29..7.......8..67.82....26.95..8..2.3..9..5.1.3..')));
+grid1 = '..3.2.6..9..3.5..1..18.64....81.29..7.......8..67.82....26.95..8..2.3..9..5.1.3..';
+values = grid_values('..3.2.6..9..3.5..1..18.64....81.29..7.......8..67.82....26.95..8..2.3..9..5.1.3..');
+display(reduce_puzzle(values));
+grid2 = '4.....8.5.3..........7......2.....6.....8.4......1.......6.3.7.5..2.....1.4......'
+values = grid_values(grid2)
+display(reduce_puzzle(values));
