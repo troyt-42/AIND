f4074fd32446fce41162268e363dd18fadedd18f
Tao_Mac <rnm2888@gmail.com>
2017-01-23 23:53:57 -0500

    Apply constraint propagation with eliminate and only-choice

--- soduku/function.py
+++ soduku/function.py
@@ -46,7 +46,24 @@ def only_choice(values):
                 values[dplaces[0]] = digit
     return values
 
+def reduce_puzzle(values):
+    solved_values = [box for box in values.keys() if len(values[box]) == 1]
+    stalled = False
+    while not stalled:
+        # Check how many boxes have a determined value
+        solved_values_before = len([box for box in values.keys() if len(values[box]) == 1])
 
-# display(grid_values('..3.2.6..9..3.5..1..18.64....81.29..7.......8..67.82....26.95..8..2.3..9..5.1.3..'));
-display(eliminate(grid_values('..3.2.6..9..3.5..1..18.64....81.29..7.......8..67.82....26.95..8..2.3..9..5.1.3..')));
-display(only_choice(eliminate(grid_values('..3.2.6..9..3.5..1..18.64....81.29..7.......8..67.82....26.95..8..2.3..9..5.1.3..'))));
+        # Your code here: Use the Eliminate Strategy
+        values = eliminate(values);
+        # Your code here: Use the Only Choice Strategy
+        values = only_choice(values);
+        # Check how many boxes have a determined value, to compare
+        solved_values_after = len([box for box in values.keys() if len(values[box]) == 1])
+        # If no new values were added, stop the loop.
+        stalled = solved_values_before == solved_values_after
+        # Sanity check, return False if there is a box with zero available values:
+        if len([box for box in values.keys() if len(values[box]) == 0]):
+            return False
+    return values
+
+display(reduce_puzzle(grid_values('..3.2.6..9..3.5..1..18.64....81.29..7.......8..67.82....26.95..8..2.3..9..5.1.3..')));
