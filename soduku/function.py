a0f558fbbc044fa202c9ba6c1f5a9bee28877b65
Tao_Mac <rnm2888@gmail.com>
2017-01-24 15:45:42 -0500

    Add third strategy: DFS (my own implementation)

--- soduku/function.py
+++ soduku/function.py
@@ -47,16 +47,22 @@ def only_choice(values):
     return values
 
 def reduce_puzzle(values):
+    """
+    Iterate eliminate() and only_choice(). If at some point, there is a box with no available values, return False.
+    If the sudoku is solved, return the sudoku.
+    If after an iteration of both functions, the sudoku remains the same, return the sudoku.
+    Input: A sudoku in dictionary form.
+    Output: The resulting sudoku in dictionary form.
+    """
     solved_values = [box for box in values.keys() if len(values[box]) == 1]
     stalled = False
     while not stalled:
         # Check how many boxes have a determined value
         solved_values_before = len([box for box in values.keys() if len(values[box]) == 1])
-
-        # Your code here: Use the Eliminate Strategy
-        values = eliminate(values);
-        # Your code here: Use the Only Choice Strategy
-        values = only_choice(values);
+        # Use the Eliminate Strategy
+        values = eliminate(values)
+        # Use the Only Choice Strategy
+        values = only_choice(values)
         # Check how many boxes have a determined value, to compare
         solved_values_after = len([box for box in values.keys() if len(values[box]) == 1])
         # If no new values were added, stop the loop.
@@ -65,9 +71,29 @@ def reduce_puzzle(values):
         if len([box for box in values.keys() if len(values[box]) == 0]):
             return False
     return values
-grid1 = '..3.2.6..9..3.5..1..18.64....81.29..7.......8..67.82....26.95..8..2.3..9..5.1.3..';
-values = grid_values('..3.2.6..9..3.5..1..18.64....81.29..7.......8..67.82....26.95..8..2.3..9..5.1.3..');
-display(reduce_puzzle(values));
+
+def solve(values):
+    "Using depth-first search and propagation, create a search tree and solve the sudoku."
+    # First, reduce the puzzle using the previous function
+    values = reduce_puzzle(values)
+    # Chose one of the unfilled square s with the fewest possibilities
+    if (values == False):
+        return False
+    lengthes = [(key,len(value)) for key, value in values.items() if len(value) > 1]
+    if (len(lengthes) > 0):
+        min_box = min(lengthes, key = lambda t: t[1])
+        # Now use recursion to solve each one of the resulting sudokus, and if one returns a value (not False), return that answer!
+        for value in values[min_box[0]]:
+            temp = values.copy()
+            temp[min_box[0]] = value
+            temp = solve(temp)
+            if ((temp != False) and (len([key for key, value in temp.items() if len(value) > 1]) == 0)):
+                return temp
+    # If you're stuck, see the solution.py tab!
+    return values;
+# grid1 = '..3.2.6..9..3.5..1..18.64....81.29..7.......8..67.82....26.95..8..2.3..9..5.1.3..';
+# values = grid_values('..3.2.6..9..3.5..1..18.64....81.29..7.......8..67.82....26.95..8..2.3..9..5.1.3..');
+# display(reduce_puzzle(values));
 grid2 = '4.....8.5.3..........7......2.....6.....8.4......1.......6.3.7.5..2.....1.4......'
 values = grid_values(grid2)
-display(reduce_puzzle(values));
+display(solve(values));
